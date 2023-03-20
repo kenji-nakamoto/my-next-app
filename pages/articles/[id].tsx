0d06ed6bc1e4f7d3b6e9be20b53c7c2fed30e092
kenji-nakamoto <k.nakamoto.work@gmail.com>
2023-03-20 18:12:44 +0900

    add：記事一覧、記事詳細を実装

---
+++ pages/articles/[id].tsx
@@ -0,0 +1,42 @@
+import { client } from 'libs/cms-client'
+import type { NextPage, GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
+import { Article } from 'types/articles'
+
+type Props = {
+  article: Article
+}
+
+const ArticleId: NextPage<Props> = ({ article }) => {
+  return (
+    <main>
+      <h1>{article.title}</h1>
+      <p>{article.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${article.body}`,
+        }}
+      />
+    </main>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'articles' }),
+    paths = data.contents.map((content: Article) => `/articles/${content.id}`)
+
+  return { paths, fallback: false }
+}
+
+export const getStaticProps: GetStaticProps<Props> = async (context: GetStaticPropsContext) => {
+  const { id } = context.params || {},
+    contentId = Array.isArray(id) ? id[0] : id,
+    data = await client.get({ endpoint: 'articles', contentId: contentId })
+
+  return {
+    props: {
+      article: data,
+    },
+  }
+}
+
+export default ArticleId
---
+++ types/articles.ts
@@ -0,0 +1,9 @@
+export type Article = {
+  id: string
+  body: string
+  title: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+}
